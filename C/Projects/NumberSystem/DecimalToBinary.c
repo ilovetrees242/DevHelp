8206fe1f4a182d1f62841425b568d26f53e80c18
ilovetrees242 <riyrty594@gmail.com>
2025-12-12 16:01:17 +0530

    added new C project

---
+++ C/Projects/NumberSystem/DecimalToBinary.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+
+	int DecNum;
+	char BinaryNum[128] = "";
+
+	printf("Enter a decimal number: ");
+	scanf("%d", &DecNum);
+	
+	while(DecNum != 0){
+		if(DecNum % 2 == 1){
+			sprintf(BinaryNum + strlen(BinaryNum), "%d", 1);
+			DecNum /= 2;
+		}
+		else if(DecNum % 2 == 0)
+			sprintf(BinaryNum + strlen(BinaryNum), "%d", 1);
+			DecNum /= 2;
+		}
+	}
+	printf("%s", BinaryNum);
+}
---
+++ C/Projects/TextToBinary.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char* DecToBin(int DecNum){
+	char *BinNum = malloc(33 * sizeof(char));
+	char *result = malloc(33 * sizeof(char));
+	while(DecNum != 0){
+		if(DecNum % 2 == 0){
+			sprintf(BinNum + strlen(BinNum), "%d", DecNum % 2);
+			DecNum /= 2;
+		}
+		else if(DecNum % 2 == 1){
+			sprintf(BinNum + strlen(BinNum), "%d", DecNum % 2);
+			DecNum /= 2;
+		}
+	}
+	const int length = strlen(BinNum);
+	for(int i = length; i > 0; i--){
+		result[length - i] = BinNum[i - 1]; 
+	}
+	return result;
+	free(BinNum);
+	free(result);
+}
+int main(){
+	char Text[512] = "";
+	int CharBuffer;
+	int Counter = 0;
+	printf("Enter any text (max characters 512): ");
+	fgets(Text, sizeof(Text), stdin);
+	Text[strlen(Text) - 1] = '\0';
+	for(int i = 0; i < strlen(Text); i++){
+		CharBuffer = Text[i];
+		printf("%s ", DecToBin(CharBuffer));
+		Counter++;
+		if(Counter == 5){
+			Counter = 0;
+			printf("\n");
+		}
+	}
+	printf("\n");
+	return 0;
+}
