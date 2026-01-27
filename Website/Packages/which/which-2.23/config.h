00b3ecb4d2f469416beb94fce8cdcccf8441822d
ilovetrees242 <riyrty594@gmail.com>
2026-01-27 14:02:25 +0530

    Fixed some bugs in which build script

---
+++ Website/Packages/which/which-2.23/config.h
@@ -0,0 +1,137 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to the type of elements in the array argument to 'getgroups'.
+   Usually this is either 'int' or 'gid_t'. */
+#define GETGROUPS_T gid_t
+
+/* Define to 1 if you have the 'eaccess' function. */
+#define HAVE_EACCESS 1
+
+/* Define to 1 if you have the 'getcwd' function. */
+#define HAVE_GETCWD 1
+
+/* Define to 1 if you have the 'getgroups' function. */
+#define HAVE_GETGROUPS 1
+
+/* Define to 1 if you have the 'getopt_long' function. */
+#define HAVE_GETOPT_LONG 1
+
+/* Define to 1 if you have the 'getpwent' function. */
+#define HAVE_GETPWENT 1
+
+/* Define to 1 if you have the 'getpwnam' function. */
+#define HAVE_GETPWNAM 1
+
+/* Define to 1 if you have the 'getpwuid' function. */
+#define HAVE_GETPWUID 1
+
+/* Define if getpw functions are declared in pwd.h */
+#define HAVE_GETPW_DECLS /**/
+
+/* Define to 1 if you have the 'getwd' function. */
+#define HAVE_GETWD 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <libiberty.h> header file. */
+/* #undef HAVE_LIBIBERTY_H */
+
+/* Define to 1 if you have the 'memcpy' function. */
+#define HAVE_MEMCPY 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <pwd.h> header file. */
+#define HAVE_PWD_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the 'strchr' function. */
+#define HAVE_STRCHR 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the 'sysconf' function. */
+#define HAVE_SYSCONF 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define if xmalloc is not available in libiberty */
+#define NEED_XMALLOC 1
+
+/* Name of package */
+#define PACKAGE "which"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "which-bugs@gnu.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "which"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "which 2.23"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "which"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.23"
+
+/* Define to 1 if the 'S_IS*' macros in <sys/stat.h> do not work properly. */
+/* #undef STAT_MACROS_BROKEN */
+
+/* Define to 1 if all of the C89 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "2.23"
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+/* #undef _FILE_OFFSET_BITS */
+
+/* Define to 1 on platforms where this makes off_t a 64-bit type. */
+/* #undef _LARGE_FILES */
+
+/* Number of bits in time_t, on hosts where this is settable. */
+/* #undef _TIME_BITS */
+
+/* Define to 1 on platforms where this makes time_t a 64-bit type. */
+/* #undef __MINGW_USE_VC2005_COMPAT */
+
+/* Define to empty if 'const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define as 'int' if <sys/types.h> doesn't define. */
+/* #undef gid_t */
+
+/* Define as 'unsigned int' if <stddef.h> doesn't define. */
+/* #undef size_t */
+
+/* Define as 'int' if <sys/types.h> doesn't define. */
+/* #undef uid_t */
---
+++ Website/Packages/which/which-2.23/cwm4/configure_ac_bottom.m4
@@ -0,0 +1,4 @@
+m4_sinclude([m4/submodules.m4])
+
+AC_CONFIG_FILES([Makefile])
+AC_OUTPUT
