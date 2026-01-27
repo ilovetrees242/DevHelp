544f7ff753709fc7ce3746526e0f9a56be92415e
ilovetrees242 <riyrty594@gmail.com>
2026-01-27 15:10:45 +0530

    Fixed some minor bugs in which build script

--- Website/Packages/which/which-2.23/cwm4/cmake/CW_CHECK_BMI2_SUPPORT.c
+++
@@ -1,7 +0,0 @@
-#include "stdio.h"
-
-int main()
-{
-  __builtin_cpu_init();
-  printf("%c", __builtin_cpu_supports("bmi2") ? '1' : '0');
-}
--- Website/Packages/which/which-2.23/cwm4/configure_ac_bottom.m4
+++
@@ -1,4 +0,0 @@
-m4_sinclude([m4/submodules.m4])
-
-AC_CONFIG_FILES([Makefile])
-AC_OUTPUT
